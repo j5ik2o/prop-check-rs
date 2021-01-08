24b010abbbb59f8ece90205f90838920974121ef
Junichi Kato <j5ik2o@gmail.com>
2021-01-08 16:21:24 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,50 @@ mod state;
 const COUNT: u32 = 100000000;
 
 fn ints1() {
-  let rng: RNG = RNG::new();
-  let (rands, _) = rng.ints1(COUNT); // stackが溢れる
-  println!("{}: {}...{}", COUNT, rands.get(0).unwrap(), rands.get(rands.len() - 1).unwrap());
+    let rng: RNG = RNG::new();
+    let (rands, _) = rng.ints1(COUNT); // stackが溢れる
+    println!(
+        "{}: {}...{}",
+        COUNT,
+        rands.get(0).unwrap(),
+        rands.get(rands.len() - 1).unwrap()
+    );
 }
 
 fn ints2() {
-  let rng: RNG = RNG::new();
-  let (rands, _) = rng.ints2(COUNT); // stackが溢れる
-  println!("{}: {}...{}", COUNT, rands.get(0).unwrap(), rands.get(rands.len() - 1).unwrap());
+    let rng: RNG = RNG::new();
+    let (rands, _) = rng.ints2(COUNT); // stackが溢れる
+    println!(
+        "{}: {}...{}",
+        COUNT,
+        rands.get(0).unwrap(),
+        rands.get(rands.len() - 1).unwrap()
+    );
 }
 
 fn ints3() {
-  let rng: RNG = RNG::new();
-  let (rands, _) = rng.ints3(COUNT);
-  println!("{}: {}...{}", COUNT, rands.get(0).unwrap(), rands.get(rands.len() - 1).unwrap());
+    let rng: RNG = RNG::new();
+    let (rands, _) = rng.ints3(COUNT);
+    println!(
+        "{}: {}...{}",
+        COUNT,
+        rands.get(0).unwrap(),
+        rands.get(rands.len() - 1).unwrap()
+    );
 }
 
 fn ints_f() {
-  let rands_f = RNG::ints_f(COUNT);
-  let rng: RNG = RNG::new();
-  let (rands, _) = rands_f(rng); // stackが溢れる
-  println!("{}: {}...{}", COUNT, rands.get(0).unwrap(), rands.get(rands.len() - 1).unwrap());
+    let rands_f = RNG::ints_f(COUNT);
+    let rng: RNG = RNG::new();
+    let (rands, _) = rands_f(rng); // stackが溢れる
+    println!(
+        "{}: {}...{}",
+        COUNT,
+        rands.get(0).unwrap(),
+        rands.get(rands.len() - 1).unwrap()
+    );
 }
 
 fn main() {
-  ints3();
+    ints3();
 }
--- src/state.rs
+++ src/state.rs
@@ -1,233 +1,245 @@
 pub trait NextRandValue {
-  fn next_i32(&self) -> (i32, Self);
+    fn next_i32(&self) -> (i32, Self);
 
-  fn next_u32(&self) -> (u32, Self) where Self: Sized {
-    let (i, r) = self.next_i32();
-    (if i < 0 { -(i + 1) as u32 } else { i as u32 }, r)
-  }
+    fn next_u32(&self) -> (u32, Self)
+    where
+        Self: Sized,
+    {
+        let (i, r) = self.next_i32();
+        (if i < 0 { -(i + 1) as u32 } else { i as u32 }, r)
+    }
 
-  fn next_f32(&self) -> (f32, Self) where Self: Sized {
-    let (i, r) = self.next_i32();
-    (i as f32 / (std::i32::MAX as f32 + 1.0f32), r)
-  }
+    fn next_f32(&self) -> (f32, Self)
+    where
+        Self: Sized,
+    {
+        let (i, r) = self.next_i32();
+        (i as f32 / (std::i32::MAX as f32 + 1.0f32), r)
+    }
 }
 
 #[derive(Clone, Debug)]
 pub struct RNG {
-  seed: i64,
+    seed: i64,
 }
 
 type DynRand<'a, A> = dyn Fn(RNG) -> (A, RNG) + 'a;
 type BoxRand<'a, A> = Box<DynRand<'a, A>>;
 
 impl NextRandValue for RNG {
-  fn next_i32(&self) -> (i32, Self) {
-    let new_seed = self.seed.wrapping_mul(0x5DEECE66D) & 0xFFFFFFFFFFFF;
-    let next_rng = RNG { seed: new_seed };
-    let n = (new_seed >> 16) as i32;
-    (n, next_rng)
-  }
+    fn next_i32(&self) -> (i32, Self) {
+        let new_seed = self.seed.wrapping_mul(0x5DEECE66D) & 0xFFFFFFFFFFFF;
+        let next_rng = RNG { seed: new_seed };
+        let n = (new_seed >> 16) as i32;
+        (n, next_rng)
+    }
 }
 
 impl RNG {
-  pub fn new() -> Self {
-    RNG { seed: i64::MAX }
-  }
-
-  pub fn int_double(&self) -> ((i32, f32), Self) {
-    let (i, r1) = self.next_i32();
-    let (d, r2) = r1.next_f32();
-    ((i, d), r2)
-  }
-
-  pub fn double_int(&self) -> ((f32, i32), Self) {
-    let ((i, d), r) = self.int_double();
-    ((d, i), r)
-  }
-
-  pub fn double_3(&self) -> ((f32, f32, f32), Self) {
-    let (d1, r1) = self.next_f32();
-    let (d2, r2) = r1.next_f32();
-    let (d3, r3) = r2.next_f32();
-    ((d1, d2, d3), r3)
-  }
-
-  pub fn ints1(self, count: u32) -> (Vec<i32>, Self) {
-    if count == 0 {
-      (vec![], self)
-    } else {
-      let (x, new_rng) = self.next_i32();
-      let (mut acc, new_rng) = new_rng.ints1(count - 1);
-      acc.push(x);
-      (acc, new_rng)
+    pub fn new() -> Self {
+        RNG { seed: i64::MAX }
     }
-  }
 
-  pub fn ints2(self, count: u32) -> (Vec<i32>, Self) {
-    fn go(count: u32, rng: RNG, mut acc: Vec<i32>) -> (Vec<i32>, RNG) {
-      if count == 0 {
-        (acc, rng)
-      } else {
-        let (x, new_rng) = rng.next_i32();
-        acc.push(x);
-        go(count - 1, new_rng, acc)
-      }
+    pub fn int_double(&self) -> ((i32, f32), Self) {
+        let (i, r1) = self.next_i32();
+        let (d, r2) = r1.next_f32();
+        ((i, d), r2)
     }
-    go(count, self, vec![])
-  }
 
-  pub fn ints3(self, count: u32) -> (Vec<i32>, Self) {
-    let mut index = count;
-    let mut acc = vec![];
-    let mut current_rng = self;
-    while index > 0 {
-      let (x, new_rng) = current_rng.next_i32();
-      acc.push(x);
-      index -= 1;
-      current_rng = new_rng;
+    pub fn double_int(&self) -> ((f32, i32), Self) {
+        let ((i, d), r) = self.int_double();
+        ((d, i), r)
     }
-    (acc, current_rng)
-  }
 
-  pub fn ints_f<'a>(count: u32) -> BoxRand<'a, Vec<i32>> {
-    let mut fs: Vec<Box<DynRand<i32>>> = Vec::with_capacity(count as usize);
-    fs.resize_with(count as usize, || Self::int_value());
-    Self::sequence(fs)
-  }
+    pub fn double_3(&self) -> ((f32, f32, f32), Self) {
+        let (d1, r1) = self.next_f32();
+        let (d2, r2) = r1.next_f32();
+        let (d3, r3) = r2.next_f32();
+        ((d1, d2, d3), r3)
+    }
 
-  pub fn unit<'a, A: Clone + 'a>(a: A) -> BoxRand<'a, A> {
-    Box::new(move |rng| (a.clone(), rng))
-  }
-
-  pub fn sequence<'a, A: Clone + 'a, F>(fs: Vec<F>) -> BoxRand<'a, Vec<A>>
-    where
-      F: Fn(RNG) -> (A, RNG) + 'a,
-  {
-    let unit = Self::unit(Vec::<A>::new());
-    let result = fs.into_iter().fold(unit, |acc, e| {
-      Self::map2(acc, e, |mut a, b| {
-        a.push(b);
-        a
-      })
-    });
-    result
-  }
-
-  pub fn int_value<'a>() -> BoxRand<'a, i32> {
-    Box::new(move |rng| rng.next_i32())
-  }
-
-  pub fn double_value<'a>() -> BoxRand<'a, f32> {
-    Box::new(move |rng| rng.next_f32())
-  }
-
-  pub fn map<'a, A, B, F1, F2>(s: F1, f: F2) -> BoxRand<'a, B>
-    where
-      F1: Fn(RNG) -> (A, RNG) + 'a,
-      F2: Fn(A) -> B + 'a,
-  {
-    Box::new(move |rng| {
-      let (a, rng2) = s(rng);
-      (f(a), rng2)
-    })
-  }
-
-  pub fn map2<'a, F1, F2, F3, A, B, C>(ra: F1, rb: F2, f: F3) -> BoxRand<'a, C>
-    where
-      F1: Fn(RNG) -> (A, RNG) + 'a,
-      F2: Fn(RNG) -> (B, RNG) + 'a,
-      F3: Fn(A, B) -> C + 'a,
-  {
-    Box::new(move |rng| {
-      let (a, r1) = ra(rng);
-      let (b, r2) = rb(r1);
-      (f(a, b), r2)
-    })
-  }
-
-  pub fn both<'a, F1, F2, A, B>(ra: F1, rb: F2) -> BoxRand<'a, (A, B)>
-    where
-      F1: Fn(RNG) -> (A, RNG) + 'a,
-      F2: Fn(RNG) -> (B, RNG) + 'a,
-  {
-    Self::map2(ra, rb, |a, b| (a, b))
-  }
-
-  pub fn rand_int_double<'a>() -> BoxRand<'a, (i32, f32)> {
-    Self::both(Self::int_value(), Self::double_value())
-  }
-
-  pub fn rand_double_int<'a>() -> BoxRand<'a, (f32, i32)> {
-    Self::both(Self::double_value(), Self::int_value())
-  }
-
-  pub fn flat_map<'a, A, B, F, GF, BF>(f: F, g: GF) -> BoxRand<'a, B>
-    where
-      F: Fn(RNG) -> (A, RNG) + 'a,
-      BF: Fn(RNG) -> (B, RNG),
-      GF: Fn(A) -> BF + 'a,
-  {
-    Box::new(move |rng| {
-      let (a, r1) = f(rng);
-      (g(a))(r1)
-    })
-  }
-
-  pub fn non_negative_less_than<'a>(n: u32) -> BoxRand<'a, u32> {
-    Self::flat_map(
-      |rng| rng.next_u32(),
-      move |i| {
-        let m = i % n;
-        if i + (n - 1) - m >= 0 {
-          Self::unit(m)
+    pub fn ints1(self, count: u32) -> (Vec<i32>, Self) {
+        if count == 0 {
+            (vec![], self)
         } else {
-          Self::non_negative_less_than(n)
+            let (x, new_rng) = self.next_i32();
+            let (mut acc, new_rng) = new_rng.ints1(count - 1);
+            acc.push(x);
+            (acc, new_rng)
         }
-      },
-    )
-  }
+    }
+
+    pub fn ints2(self, count: u32) -> (Vec<i32>, Self) {
+        fn go(count: u32, rng: RNG, mut acc: Vec<i32>) -> (Vec<i32>, RNG) {
+            if count == 0 {
+                (acc, rng)
+            } else {
+                let (x, new_rng) = rng.next_i32();
+                acc.push(x);
+                go(count - 1, new_rng, acc)
+            }
+        }
+        go(count, self, vec![])
+    }
+
+    pub fn ints3(self, count: u32) -> (Vec<i32>, Self) {
+        let mut index = count;
+        let mut acc = vec![];
+        let mut current_rng = self;
+        while index > 0 {
+            let (x, new_rng) = current_rng.next_i32();
+            acc.push(x);
+            index -= 1;
+            current_rng = new_rng;
+        }
+        (acc, current_rng)
+    }
+
+    pub fn ints_f<'a>(count: u32) -> BoxRand<'a, Vec<i32>> {
+        let mut fs: Vec<Box<DynRand<i32>>> = Vec::with_capacity(count as usize);
+        fs.resize_with(count as usize, || Self::int_value());
+        Self::sequence(fs)
+    }
+
+    pub fn unit<'a, A: Clone + 'a>(a: A) -> BoxRand<'a, A> {
+        Box::new(move |rng| (a.clone(), rng))
+    }
+
+    pub fn sequence<'a, A: Clone + 'a, F>(fs: Vec<F>) -> BoxRand<'a, Vec<A>>
+    where
+        F: Fn(RNG) -> (A, RNG) + 'a,
+    {
+        let unit = Self::unit(Vec::<A>::new());
+        let result = fs.into_iter().fold(unit, |acc, e| {
+            Self::map2(acc, e, |mut a, b| {
+                a.push(b);
+                a
+            })
+        });
+        result
+    }
+
+    pub fn int_value<'a>() -> BoxRand<'a, i32> {
+        Box::new(move |rng| rng.next_i32())
+    }
+
+    pub fn double_value<'a>() -> BoxRand<'a, f32> {
+        Box::new(move |rng| rng.next_f32())
+    }
+
+    pub fn map<'a, A, B, F1, F2>(s: F1, f: F2) -> BoxRand<'a, B>
+    where
+        F1: Fn(RNG) -> (A, RNG) + 'a,
+        F2: Fn(A) -> B + 'a,
+    {
+        Box::new(move |rng| {
+            let (a, rng2) = s(rng);
+            (f(a), rng2)
+        })
+    }
+
+    pub fn map2<'a, F1, F2, F3, A, B, C>(ra: F1, rb: F2, f: F3) -> BoxRand<'a, C>
+    where
+        F1: Fn(RNG) -> (A, RNG) + 'a,
+        F2: Fn(RNG) -> (B, RNG) + 'a,
+        F3: Fn(A, B) -> C + 'a,
+    {
+        Box::new(move |rng| {
+            let (a, r1) = ra(rng);
+            let (b, r2) = rb(r1);
+            (f(a, b), r2)
+        })
+    }
+
+    pub fn both<'a, F1, F2, A, B>(ra: F1, rb: F2) -> BoxRand<'a, (A, B)>
+    where
+        F1: Fn(RNG) -> (A, RNG) + 'a,
+        F2: Fn(RNG) -> (B, RNG) + 'a,
+    {
+        Self::map2(ra, rb, |a, b| (a, b))
+    }
+
+    pub fn rand_int_double<'a>() -> BoxRand<'a, (i32, f32)> {
+        Self::both(Self::int_value(), Self::double_value())
+    }
+
+    pub fn rand_double_int<'a>() -> BoxRand<'a, (f32, i32)> {
+        Self::both(Self::double_value(), Self::int_value())
+    }
+
+    pub fn flat_map<'a, A, B, F, GF, BF>(f: F, g: GF) -> BoxRand<'a, B>
+    where
+        F: Fn(RNG) -> (A, RNG) + 'a,
+        BF: Fn(RNG) -> (B, RNG),
+        GF: Fn(A) -> BF + 'a,
+    {
+        Box::new(move |rng| {
+            let (a, r1) = f(rng);
+            (g(a))(r1)
+        })
+    }
+
+    pub fn non_negative_less_than<'a>(n: u32) -> BoxRand<'a, u32> {
+        Self::flat_map(
+            |rng| rng.next_u32(),
+            move |i| {
+                let m = i % n;
+                if i + (n - 1) - m >= 0 {
+                    Self::unit(m)
+                } else {
+                    Self::non_negative_less_than(n)
+                }
+            },
+        )
+    }
 }
 
 mod state {
-  use std::rc::Rc;
+    use std::rc::Rc;
 
-  use crate::state::RNG;
+    use crate::state::RNG;
 
-  struct State<'a, S, A> {
-    run: Box<dyn Fn(S) -> (A, S) + 'a>,
-  }
-
-  impl<'a, S, A> State<'a, S, A> {
-    pub fn pure<'b, X: Clone + 'b>(x: X) -> State<'b, S, X> {
-      State {
-        run: box move |s| (x.clone(), s),
-      }
+    struct State<'a, S, A> {
+        run: Box<dyn Fn(S) -> (A, S) + 'a>,
     }
 
-    pub fn fmap<'b, B: Clone + 'b, F>(&'b self, f: F) -> State<'b, S, B> where F: Fn(A) -> B + 'b {
-      self.bind(move |a| Self::pure(f(a)))
-    }
+    impl<'a, S, A> State<'a, S, A> {
+        pub fn pure<'b, X: Clone + 'b>(x: X) -> State<'b, S, X> {
+            State {
+                run: box move |s| (x.clone(), s),
+            }
+        }
 
-    pub fn bind<'b, B, F>(&'b self, f: F) -> State<'b, S, B> where F: Fn(A) -> State<'b, S, B> + 'b {
-      State {
-        run: box move |s| {
-          let (b, s1) = (self.run)(s);
-          (f(b).run)(s1)
-        },
-      }
+        pub fn fmap<'b, B: Clone + 'b, F>(&'b self, f: F) -> State<'b, S, B>
+        where
+            F: Fn(A) -> B + 'b,
+        {
+            self.bind(move |a| Self::pure(f(a)))
+        }
+
+        pub fn bind<'b, B, F>(&'b self, f: F) -> State<'b, S, B>
+        where
+            F: Fn(A) -> State<'b, S, B> + 'b,
+        {
+            State {
+                run: box move |s| {
+                    let (b, s1) = (self.run)(s);
+                    (f(b).run)(s1)
+                },
+            }
+        }
     }
-  }
 }
 
 #[cfg(test)]
 mod tests {
-  use crate::state::{NextRandValue, RNG};
+    use crate::state::{NextRandValue, RNG};
 
-  #[test]
-  fn next_int() {
-    let (v1, r1) = RNG::new().next_i32();
-    println!("{:?}", v1);
-    let (v2, _) = r1.next_u32();
-    println!("{:?}", v2);
-  }
+    #[test]
+    fn next_int() {
+        let (v1, r1) = RNG::new().next_i32();
+        println!("{:?}", v1);
+        let (v2, _) = r1.next_u32();
+        println!("{:?}", v2);
+    }
 }
