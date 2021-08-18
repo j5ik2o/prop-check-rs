86457e8b449f61045da6c6825ced71d1f9dfc6fd
Junichi Kato <j5ik2o@gmail.com>
2021-08-18 18:56:39 +0900

    add a file.

--- src/gen/one.rs
+++ src/gen/one.rs
@@ -15,3 +15,63 @@ impl One for u64 {
         Gens::one_u64()
     }
 }
+
+impl One for i32 {
+    fn one() -> Gen<Self> {
+        Gens::one_i32()
+    }
+}
+
+impl One for u32 {
+    fn one() -> Gen<Self> {
+        Gens::one_u32()
+    }
+}
+
+impl One for i16 {
+    fn one() -> Gen<Self> {
+        Gens::one_i16()
+    }
+}
+
+impl One for u16 {
+    fn one() -> Gen<Self> {
+        Gens::one_u16()
+    }
+}
+
+impl One for i8 {
+    fn one() -> Gen<Self> {
+        Gens::one_i8()
+    }
+}
+
+impl One for u8 {
+    fn one() -> Gen<Self> {
+        Gens::one_u8()
+    }
+}
+
+impl One for char {
+    fn one() -> Gen<Self> {
+        Gens::one_char()
+    }
+}
+
+impl One for bool {
+    fn one() -> Gen<Self> {
+        Gens::one_bool()
+    }
+}
+
+impl One for f64 {
+    fn one() -> Gen<Self> {
+        Gens::one_f64()
+    }
+}
+
+impl One for f32 {
+    fn one() -> Gen<Self> {
+        Gens::one_f32()
+    }
+}
